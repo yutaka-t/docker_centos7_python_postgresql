24ea38e9b185ac23978bf664d61f5e6a6b156c97
Yutaka <yutaka.tanoue@gmail.com>
2021-08-22 18:00:16 +0900

    接続用情報を変数にした

--- python_src/test.py
+++ python_src/test.py
@@ -1,8 +1,14 @@
 import psycopg2
 
-postgres_ip = '172.24.0.2'
+# Connection information
+postgres_ip = '172.19.0.2'
+port = '5432'
+db_name = 'admin'
+user_name = 'admin'
+password = 'admin'
 
-connection = psycopg2.connect(f"host={postgres_ip} port=5432 dbname=admin user=admin password=admin")
+
+connection = psycopg2.connect(f"host={postgres_ip} port={port} dbname={db_name} user={user_name} password={password}")
 cur = connection.cursor()
 cur.execute("select * from work_users")
 for row in cur:
