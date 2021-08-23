bd8e6d795d39f6ae430050276036b2cff558d3fd
Yutaka <yutaka.tanoue@gmail.com>
2021-08-23 20:35:47 +0900

    接続確認スクリプトに、接続失敗時の処理追加

--- python_src/test.py
+++ python_src/test.py
@@ -1,16 +1,22 @@
 import psycopg2
 
 # Connection information
-postgres_ip = '172.19.0.2'
+postgres_ip = '172.18.0.4'
 port = '5432'
 db_name = 'admin'
 user_name = 'admin'
 password = 'admin'
 
+try:
+    connection = psycopg2.connect(f"host={postgres_ip} port={port} dbname={db_name} user={user_name} password={password}")
+    cur = connection.cursor()
+    cur.execute("select * from work_users")
+    for row in cur:
+        print(row)
 
-connection = psycopg2.connect(f"host={postgres_ip} port={port} dbname={db_name} user={user_name} password={password}")
-cur = connection.cursor()
-cur.execute("select * from work_users")
-for row in cur:
-    print(row)
+except Exception as e:
+    print(f"\nDB接続に失敗しました。　[接続先: {postgres_ip}:{port}]")
+    print("-"*80)
+    print("<エラーメッセージ>")
+    print(e)
 
